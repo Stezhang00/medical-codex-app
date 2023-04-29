7657637f62678a0c26aa2165759c9d7246709ffc
Stezhang00 <stephanie.zhang@stonybrook.edu>
2023-04-29 22:26:18 +0000

    updated main.py

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/medical-codex-1")
-def get_medical_codex_1():
-    return {"codex": "Medical Codex 1"}
+icd10am = {
+    "A00": "Cholera",
+    "A01": "Typhoid fever",
+    "A02": "Salmonella infections",
+    "B00": "Herpesviral [herpes simplex] infections",
+    # more codes and descriptions here...
+}
 
-@app.get("/medical-codex-2")
-def get_medical_codex_2():
-    return {"codex": "Medical Codex 2"}
+snomedct = {
+    "105590001": "Malignant neoplasm of lung",
+    "105596006": "Malignant neoplasm of colon",
+    "105602007": "Malignant neoplasm of breast",
+    # more codes and descriptions here...
+}
 
+@app.get("/icd10am/{code}")
+def get_icd10am(code: str):
+    return {"code": code, "description": icd10am.get(code, "Code not found")}
+
+@app.get("/snomedct/{code}")
+def get_snomedct(code: str):
+    return {"code": code, "description": snomedct.get(code, "Code not found")}
